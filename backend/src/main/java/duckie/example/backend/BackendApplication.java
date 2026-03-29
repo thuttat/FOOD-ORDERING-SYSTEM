c48ddcd47b43d7cd82641f87dc3e27c4c4ab0c08
Trieu Duy Nguyen <duynguyendn2442@gmail.com>
2026-03-29 16:17:46 +0700

    feat: init backend project, setup MySQL and implement Login/Register APIs

--- backend/src/main/java/duckie/example/backend/BackendApplication.java
+++ backend/src/main/java/duckie/example/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package duckie.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"duckie.example.backend.entity"})
 public class BackendApplication {
 
 	public static void main(String[] args) {
